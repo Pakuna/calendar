852330a9790c99563a2cd55cb15cd5d410a759ca
Stefan Hahn <pakuna@gmx.de>
2018-08-08 17:59:28 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Kalender</title>
+    <link href="img/icon/default.png" rel="shortcut icon">
+    <link rel="stylesheet" type="text/css" href="css/reset.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+</body>
+</html>
